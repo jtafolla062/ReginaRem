3452e61a24f546cb591c0e3da98ebc74e3789460
Tafolla <jtafolla062@accitesz.com>
2021-11-05 09:39:37 -0600

    Python

---
+++ pythonProject/main.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def inicio():
+    return render_template('TerminosCondiciones.html')
+@app.route('/PoliticaPrivacidad')
+def login():
+    return render_template('PoliticaPrivacidad.html')
+@app.route('/MVV')
+def main():
+    return render_template('MVV.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
